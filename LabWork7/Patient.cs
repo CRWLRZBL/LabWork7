29c3a5e4dbf8036ccb74eda68fe3a94f6579c5e5
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:48:23 +0300

    Initial commit

---
+++ LabWork7/Patient.cs
@@ -0,0 +1,55 @@
+﻿namespace Task1
+{
+    internal class Patient
+    {
+        public string Name { get; set; }
+        public int NumberPolis { get; set; }
+        public int BirthYear { get; set; }
+
+        #region Constructors
+        public Patient() : this("Unknown", 0, 2000)
+        {
+        }
+
+        public Patient(string name, int numberPolis, int dateOfBirthday)
+        {
+            Name = name;
+            NumberPolis = numberPolis;
+            BirthYear = dateOfBirthday;
+        }
+        #endregion
+
+        #region operators
+        public static Patient operator ++(Patient patient)
+            => new Patient
+            {
+                Name = patient.Name,
+                NumberPolis = patient.NumberPolis,
+                BirthYear = patient.BirthYear + 1
+            };
+
+        public static Patient operator +(Patient patient1, Patient patient2)
+            => new Patient
+            {
+                Name = patient1.Name,
+                NumberPolis = patient1.NumberPolis,
+                BirthYear = patient1.BirthYear + patient2.BirthYear
+            };
+
+        public static bool operator true(Patient patient)
+            => patient.BirthYear >= 0;
+
+        public static bool operator false(Patient patient)
+            => patient.BirthYear < 0;
+        #endregion
+
+        #region PrintMethod
+        public void Print()
+        {
+            Console.WriteLine("Name: ", Name);
+            Console.WriteLine("Polis number: ", NumberPolis);
+            Console.WriteLine("Year of birth: ", BirthYear);
+        }
+        #endregion
+    }
+}
---
+++ LabWork7/Program.cs
@@ -0,0 +1,21 @@
+﻿using Task1;
+
+Console.WriteLine("---------------------------------------------------------");
+Patient patient1 = new("Misha", 0, 2006);
+patient1++;
+Console.WriteLine($"                        {patient1.BirthYear}  ");
+Console.WriteLine("---------------------------------------------------------");
+
+Patient patient2 = new();
+Patient patient3 = patient1 + patient2;
+Console.WriteLine($"  Name: {patient3.Name}, Number of polis: {patient3.NumberPolis}, Year of birth: {patient3.BirthYear}");
+Console.WriteLine("---------------------------------------------------------");
+
+Patient patient4 = new("Denis", 0, 2005);
+Console.WriteLine($"                         {patient4.BirthYear}");
+
+if (patient4)
+    Console.WriteLine("\t  year of birth >=0");
+else
+    Console.WriteLine("\t year of birth <0");
+Console.WriteLine("---------------------------------------------------------");
